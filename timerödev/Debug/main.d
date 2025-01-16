153753b24f066eba52b4bbf9fc716581db3644d1
dogabulbul008 <dogabulbul005@gmail.com>
2025-01-16 17:22:14 +0300

    Add files via upload

---
+++ timerödev/Debug/main.d
@@ -0,0 +1,60 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/timer.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
+
+../main.c:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/timer.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h:
+
